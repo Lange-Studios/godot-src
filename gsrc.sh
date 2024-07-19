a8fc7951890aedfd74750e3c4dca111257cce634
Travis Lange <travislange12@gmail.com>
2024-07-19 18:44:15 -0400

    nushell is now installed and run through pixi

--- gsrc.sh
+++ gsrc.sh
@@ -2,4 +2,15 @@
 
 dir="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
 
-"$dir/nu/nu.sh" "$dir/gsrc.nu" "$@"
+export PIXI_NO_PATH_UPDATE="true"
+export PIXI_VERSION=${PIXI_VERSION:-latest}
+export PIXI_HOME=${PIXI_HOME:-"$dir/gitignore/pixi"}
+
+if ! test -f "$dir/gitignore/pixi/bin/pixi"
+then
+    curl -fsSL https://pixi.sh/install.sh | bash
+fi
+
+# For some reason passing $@ directly does't work, so we assing to an intermediate args
+args=$@
+"$dir/gitignore/pixi/bin/pixi" run nu -c "source \"$dir/gsrc.nu\";main $args"
