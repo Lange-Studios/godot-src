bb236923080646d396b229862b4b4176d6006e31
Travis Lange <travislange12@gmail.com>
2024-07-21 11:12:12 -0400

    added ability to install pixi with curl or wget

--- gsrc.sh
+++ gsrc.sh
@@ -9,7 +9,16 @@ export GSRC_SCRIPT=${GSRC_SCRIPT:-"$dir/gsrc.nu"}
 
 if ! test -f "$dir/gitignore/pixi/bin/pixi"
 then
-    curl -fsSL https://pixi.sh/install.sh | bash
+    if curl --version
+    then
+        curl -fsSL https://pixi.sh/install.sh | bash
+    elif wget --version
+    then
+        wget -qO- --max-redirect=20 https://pixi.sh/install.sh | bash
+    else
+        echo "ERROR: tried to install pixi. wget or curl must be installed"
+        exit 1
+    fi
 fi
 
 # For some reason passing $@ directly does't work, so we assign to an intermediate args
